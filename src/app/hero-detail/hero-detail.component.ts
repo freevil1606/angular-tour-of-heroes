323f22da6b520df2688dd3f3ae472c768c6929f7
Gem Truong <tram.truong@technology.com>
2021-12-29 10:48:31 +0700

    tour-of-heroes: 5

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,12 +10,21 @@ import {Hero} from "../hero";
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+  hero?: Hero;
 
-  @Input() hero?: Hero;
-
-  constructor() { }
+  constructor(private heroService: HeroService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    let id = Number(this.route.snapshot.paramMap.get("id"));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,10 @@ export class HeroService {
     this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    let hero = HEROES.find(hero => id === hero.id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
